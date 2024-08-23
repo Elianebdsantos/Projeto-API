5c1877b4002d0635a47fdd7c43175f5353fa2965
ELIANE BARBOS DOS SANTOS <29487425870@info.local>
2024-08-22 21:05:52 -0300

    API

---
+++ bd.py
@@ -0,0 +1,40 @@
+#SIMULAÇÃO DO NOSSO BANCO DE DADOS
+
+Carros = [
+    #OBJETO 1
+    {
+        'id': 1,
+        'marca': "Toyota",
+        'modelo': 'Corolla',
+        'ano': 2020
+    },
+    #OBJETO 2
+    {
+        'id': 2,
+        'marca': 'Honda',
+        'modelo': 'Civic',
+        'ano': 2015        
+    },
+    #OBJETO 3
+    {
+        'id': 3,
+        'marca': 'Fiat',
+        'modelo': 'Uno',
+        'ano': 2005
+    },
+    #OBJETO 4
+    {
+        'id': 4,
+        'marca':'Volkswagem',
+        'modelo': 'Gol',
+        'ano': 1999
+    },
+    #OBJETO 5
+    {
+        'id': 5,
+        'marca':'Chevrolet',
+        'modelo': 'Corsa',
+        'ano': 1999
+    }
+
+]
